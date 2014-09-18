4a07c8663690d0c3e970d027ebf2869224939a82
Elliott O'Hara <elliott.ohara@gmail.com>
2014-09-18 14:09:13 -0500

    more work

--- testproject/testproject/settings_dev.py
+++ testproject/testproject/settings_dev.py
@@ -1,3 +1,6 @@
+import os
+from testproject.settings import BASE_DIR
+
 __author__ = 'elliottohara'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -6,7 +9,6 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': '/tmp/db.sqlite3'
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
 }
