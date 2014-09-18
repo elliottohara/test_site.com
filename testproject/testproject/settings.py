7a8dcfb0672daaa7e01fcfd0df5c2b49c3d8925a
Elliott O'Hara <elliott.ohara@gmail.com>
2014-09-18 18:56:58 -0500

    settings stuff

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -52,31 +52,6 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'testproject.urls'
 
 WSGI_APPLICATION = 'testproject.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ltjjc',
-        'USER': 'ltjjc',
-        'PASSWORD': 'ltjjc',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
-    }
-}
-
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
@@ -114,5 +89,4 @@ MEDIA_URL = '/media/'
 
 
 
-if os.environ.get('DEVELOPMENT', None):
-    from settings_dev import *
+from local_settings import *
